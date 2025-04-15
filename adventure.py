d3783108d89585dedf5c4245b49560ca0808e7bb
shun_irei <shun_irei@xon.jp>
2025-04-15 15:18:31 +0900

    0415_009_1回目

--- adventure.py
+++ adventure.py
@@ -19,32 +19,22 @@ MAX_DEFENSE = 15
 def fight_encounter():
     # 冒険の戦闘処理：冗長な処理と良くない命名を意図的に使用しています。
     print("冒険の始まりです！あなたは勇気ある戦士です。")
-    player = Player(INITIAL_HEALTH)
-    enemy = Enemy(INITIAL_HEALTH)
+    player = Player(100)
+    enemy = Enemy(100)
     print("恐ろしい敵が姿を現しました！敵の体力は {} です。".format(enemy.health))
     
     while enemy.health > 0 and player.health > 0:
-        try:
-            input_key = input("攻撃するには 'a' キーを押してください... ディフェンスするには 'd' キーを押してください。")
-            if input_key == "d":
-                defend_attack(enemy, player)
-            else:
-                attack_enemy(enemy, player)
-        except KeyboardInterrupt:
-            print("ゲームを中断します。")
-            sys.exit(0)
+        input_key = input("攻撃するには Enter キーを押してください... ディフェンスするには 'd' キーを押してください。")
+        if input_key == "d":
+            defend_attack(enemy, player)
+        else:
+            attack_enemy(enemy, player)
         
         print("-" * 40)
 
         judge_battle_result(player, enemy)
     
     print("ゲーム終了。")
-
-"""
-内容：敵を攻撃する
-パラメータ：enemy(敵)、player(プレイヤー)
-戻り値：なし
-"""
 
 def attack_enemy(enemy, player):
     player_attack = random.randint(MIN_ATTACK, MAX_ATTACK)
@@ -57,31 +47,15 @@ def attack_enemy(enemy, player):
     player.health -= enemy_attack
     print("あなたは {} のダメージを受けました。".format(enemy_attack))
     print("あなたの残り体力は {} です。".format(player.health))
-
-"""
-内容：敵の攻撃を防御する
-パラメータ：enemy(敵)、player(プレイヤー)
-戻り値：なし
-"""
 def defend_attack(enemy, player):
     print("敵の攻撃を防御しました。")
     print("あなたの残り体力は {} です。".format(player.health))
 
-"""
-内容：戦闘の勝敗を判定する
-パラメータ：player(プレイヤー)、enemy(敵)
-戻り値：True(勝利)、False(敗北)
-"""
 def judge_battle_result(player, enemy):
     if enemy.health <= 0:
         print("敵は完全に倒されました！")
-        print("あなたは勝利しました！")
-        return True
     elif player.health <= 0:
         print("あなたは倒されました。冒険はここで終わりです。")
-        return True
-    else:
-        return False
 
 if __name__ == "__main__":
     fight_encounter()
